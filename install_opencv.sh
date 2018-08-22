2b617c15e23f9e797eac65f1e82d23f3ab608095
Peter Kydas <peterkydas@gmail.com>
2018-08-22 10:20:27 +1000

    opencv script finished for ubuntu

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 OPENCV_VERSION='3.4.2'
 
 # 1. Update Ubuntu
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
+#sudo apt-get -y dist-upgrade
+#sudo apt-get -y autoremove
 
 
 # 2. Install dependencies
@@ -13,7 +15,7 @@ sudo apt-get -y autoremove
 sudo apt-get install -y build-essential cmake
 
 # OpenCV dependencies
-sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev libtbb-dev libeigen3-dev
+sudo apt-get install -y qt5-default libvtk6-dev zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libopenexr-dev libgdal-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev libtbb-dev libeigen3-dev
 
 # Python
 sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk python3-numpy
@@ -33,3 +35,7 @@ cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON 
 make -j2
 sudo make install
 sudo ldconfig
+cd ../../
+rm -rf OpenCV
+
+sudo pip3 install PyStasm
