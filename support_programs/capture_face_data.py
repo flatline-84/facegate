034d960495a947ab386969f2796a52d8c3cf0f8b
Peter Kydas <peterkydas@gmail.com>
2018-09-23 19:21:17 +1000

    finished capture program

--- support_programs/capture_face_data.py
+++ support_programs/capture_face_data.py
@@ -16,9 +16,10 @@ running = True
 # If and what to save
 save = None
 # Output folder
-out = "out"
+out = os.path.join(os.path.dirname(os.path.abspath(__file__)), "out")
 # Name for saving files
 name = None
+im = None 
 
 # Tkinter functions
 def callback():
@@ -33,23 +34,45 @@ def key(event):
 def mouse(event):
     pass
 
+def save_image(folder_name):
+
+
+    new_path = os.path.join(out, folder_name)
+    # print(new_path)
+
+    if (not os.path.isdir(new_path)):
+        os.mkdir(new_path)
+        print("Making new folder for %s!" % folder_name)
+
+    i = 0
+
+    # for f in os.listdir(new_path):
+    while os.path.exists(os.path.join(new_path, "m-" + name + "-" + "%i.png" % i)):
+        i += 1
+
+    filename = "m-" + name + "-" + str(i) + ".png"
+
+    new_file = os.path.join(new_path, filename)
+    print(new_file)
+    # ret = cv2.imwrite(new_file, im)
+
+    # with open(new_file, 'w') as f:
+        # result.save(f)
+    im.save(new_file)
+    # print("Image saved to disk")
+
 # Button callback functions
 def AngerCallback():
-    new_path = os.path.join(out, "anger")
-
-    for f in os.listdir(new_path):
-        '''you are here peter'''
-        pass
-    pass
+    save_image("anger")
 
 def NeutralCallback():
-    pass
-
+    save_image("neutral")
+    
 def ScreamCallback():
-    pass
-
+    save_image("scream")
+    
 def SmileCallback():
-    pass
+    save_image("smile")
 
 def QuitCallback():
     global running
@@ -60,10 +83,17 @@ if __name__ == '__main__':
     # Get user's name before running the program
     if (len(sys.argv) != 2):
         print("Please only add your name as a command line argument")
+        print("EG: python3 capture_face_data.py peter")
+        print("And make sure you're running this from the support_programs folder! Otherwise it'll misbehave :)")
+        print("Your pictures will be stored in a folder called 'out'")
         exit(1)
     
-    global name
+    # global name
     name = sys.argv[1]
+
+    if (not os.path.isdir(out)):
+        os.mkdir(out)
+        print("Making new folder for %s!" % out)
 
     ## Setup Tkinter
     # Tkinter drawing code
@@ -83,6 +113,7 @@ if __name__ == '__main__':
     root.bind("<Key>", key)
     root.bind("<Button-1>", mouse)
     
+    # global im
     im = Image.open('../numbered.jpg')
     tkimg = ImageTk.PhotoImage(im)
     # self.tkimg = PhotoImage(file="./numbered.jpg")
@@ -120,11 +151,15 @@ if __name__ == '__main__':
         if ret == True:
             frame = cv2.flip(frame, 1)
             # cv2.imshow('image', frame)
+            # im = frame
 
             frame = frame[...,::-1]
             image = Image.fromarray(frame)
 
+            im = image
+
             tkimg = ImageTk.PhotoImage(image)
+                    
             imglabel.configure(image=tkimg)
             imglabel.image = tkimg
 
